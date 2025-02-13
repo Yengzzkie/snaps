69cd23b284609ae3731bce4bd52a45c889d665e5
Yengzzkie <gatchalian.manuel@gmail.com>
2025-02-13 16:37:17 -0500

    migrate to @use from @import for scss files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar.jsx';
-import './main.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
