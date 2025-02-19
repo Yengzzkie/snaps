933d34ed77467df90692512e18d15fa1105c2a65
Yengzzkie <gatchalian.manuel@gmail.com>
2025-02-18 22:35:55 -0500

    add error text when no result from filter is found

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import { useState, useEffect } from "react";
 import Filters from "./components/Filters/Filters";
 
 function App() {
-  const [filteredPhotos, setFilteredPhotos] = useState(photos)
+  const [filteredPhotos, setFilteredPhotos] = useState(photos);
   const [selectedTag, setSelectedTag] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
 
   // logic for setting the selected filter tags
   function getSelectedTag(tag) {
-
     // check if the selected tags already exists in the selectedTag array
     const isExist = selectedTag.includes(tag);
 
@@ -25,16 +24,24 @@ function App() {
   }
 
   useEffect(() => {
-    setFilteredPhotos(photos.filter((photo) => selectedTag.every((tag) => photo.tags.includes(tag)))); // filter the photos based on the selected tags
-  }, [selectedTag]) // use selectedTag as dependency so when it changes, the setFilterdPhotos will update too
+    setFilteredPhotos(
+      photos.filter((photo) =>
+        selectedTag.every((tag) => photo.tags.includes(tag))
+      )
+    ); // filter the photos based on the selected tags
+  }, [selectedTag]); // use selectedTag as dependency so when it changes, the setFilterdPhotos will update too
 
   return (
     <>
       {/* pass the isOpen state variable and setIsOpen for navbar to handle the opening of the filter dropdown */}
-      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} /> 
+      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
 
       <main>
-        <Filters isOpen={isOpen} selectedTag={selectedTag} getSelectedTag={getSelectedTag} />
+        <Filters
+          isOpen={isOpen}
+          selectedTag={selectedTag}
+          getSelectedTag={getSelectedTag}
+        />
         <div className="container">
           <section>
             <h4 className="hero__header">Our Mission:</h4>
@@ -48,13 +55,17 @@ function App() {
           </section>
 
           <section className={`card-container ${isOpen ? "two-cols" : ""}`}>
-            {filteredPhotos.map((photo) => (
-              <Card key={photo.id} props={photo} />
-            ))}
+            {filteredPhotos.length === 0 ? (
+              <p className="no-result-text">No results found</p>
+            ) : (
+              filteredPhotos.map((photo) => (
+                <Card key={photo.id} props={photo} />
+              ))
+            )}
           </section>
         </div>
       </main>
-      
+
       <Footer />
     </>
   );
