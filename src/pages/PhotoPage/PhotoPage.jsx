ac83f2e09323f1e9f8e1b02e3399db08cafcd5e3
Yengzzkie <gatchalian.manuel@gmail.com>
2025-02-25 21:38:33 -0500

    add photo card layout

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -1,6 +1,9 @@
+import "./PhotoPage.scss";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import LikeOutline from "../../assets/images/Like_Outline.svg?react";
 import Navbar from "../../components/Navbar/Navbar";
+import Tags from "../../components/Tags/Tags";
 import axios from "axios";
 
 const PhotoPage = () => {
@@ -10,13 +13,14 @@ const PhotoPage = () => {
 
   async function getPhotoData() {
     try {
-      const response = await axios.get(`${API_URL}photos/${id}?api_key=${import.meta.env.VITE_API_KEY}`);
+      const response = await axios.get(
+        `${API_URL}photos/${id}?api_key=${import.meta.env.VITE_API_KEY}`
+      );
 
       console.log(response.data);
       setPhotoData(response.data);
     } catch (error) {
       console.error({ error });
-      throw new Error("Failed to get photo data:", error);
     }
   }
 
@@ -27,17 +31,38 @@ const PhotoPage = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <Navbar />
-      Photo ID: {id}
-      {photoData && (
-        <div>
-          <p>{photoData?.id}</p>
-          <img src={photoData.photo} alt={photoData.photoDescription} />
-          <p>{photoData.photoDescription}</p>
-        </div>
-      )}
-    </div>
+
+      <div className="photo-page-container">
+        {photoData && (
+          <div className="image-card">
+            <img src={photoData.photo} alt={photoData.photoDescription} />
+
+            {/* tags container */}
+            <div className="tags-container">
+              {photoData.tags?.map((tag) => (
+                <Tags key={tag.id} text={tag} cn={"non-clickable-tag"} />
+              ))}
+            </div>
+
+            {/* likes and date container */}
+            <div className="likes-date-container">
+              <div className="likes">
+                <button>
+                  <LikeOutline />
+                </button>
+                <p>{photoData.likes} likes</p>
+              </div>
+              <p>{new Date(photoData.timestamp).toLocaleDateString()}</p>
+            </div>
+
+            {/* photographer name */}
+            <p className="photographer-name">Photo by  {photoData.photographer}</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
