f287bb70b28fab1b27d80cb79d6f380ffa8ac992
Yengzzkie <gatchalian.manuel@gmail.com>
2025-02-25 20:12:30 -0500

    add conditional rendering of photoData to make sure it is not null

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -5,34 +5,38 @@ import axios from "axios";
 const PhotoPage = () => {
   const { id } = useParams();
   const [photoData, setPhotoData] = useState(null);
-
+  const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com/";
 
   async function getPhotoData() {
-    const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com/";
     try {
-      const response = await axios.get(`${API_URL}photos/${id}?api_key=${import.meta.env.API_KEY}`)
+      const response = await axios.get(`${API_URL}photos/${id}?api_key=${import.meta.env.VITE_API_KEY}`);
 
-      console.log(response.data)
+      console.log(response.data);
       setPhotoData(response.data);
     } catch (error) {
-      console.error({ error })
-      throw new Error("Failed to get photo data:", error)
+      console.error({ error });
+      throw new Error("Failed to get photo data:", error);
     }
   }
 
   // fetch photo data on mount
   useEffect(() => {
     getPhotoData();
-    console.log(photoData)
+    console.log(photoData);
   }, []);
 
   return (
-    <div>Photo ID: {id}
-    <p>{photoData?.id}</p>
-    <img src={photoData.photo} alt={photoData.photoDescription} />
-    <p>{photoData.photoDescription}</p>
+    <div>
+      Photo ID: {id}
+      {photoData && (
+        <div>
+          <p>{photoData?.id}</p>
+          <img src={photoData.photo} alt={photoData.photoDescription} />
+          <p>{photoData.photoDescription}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PhotoPage
+export default PhotoPage;
