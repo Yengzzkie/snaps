92a4e38cbe866d7b6a07bae0e409919a8c5d4d13
Yengzzkie <gatchalian.manuel@gmail.com>
2025-02-27 19:55:40 -0500

    add comment component

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import LikeOutline from "../../assets/images/Like_Outline.svg?react";
 import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer"
+import Footer from "../../components/Footer/Footer";
 import Tags from "../../components/Tags/Tags";
 import axios from "axios";
+import Comment from "../../components/Comment/Comment";
 
 const PhotoPage = () => {
   const { id } = useParams();
@@ -13,7 +14,7 @@ const PhotoPage = () => {
   const [comments, setComments] = useState([]);
   const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com/";
 
-  // function to get the comments for the photo 
+  // function to get the comments for the photo
   async function getComments() {
     try {
       const response = await axios.get(
@@ -65,7 +66,7 @@ const PhotoPage = () => {
               ))}
             </div>
 
-            {/* likes and date container */}
+            {/* likes and date section */}
             <div className="likes-date-container">
               <div className="likes">
                 <button>
@@ -84,23 +85,21 @@ const PhotoPage = () => {
         )}
       </div>
 
-      {/* comments container */}
+      {/* comments section */}
       <div>
         {comments && (
           <div className="comments-container">
             {/* if the comments is greater than one render Comments otherwise Comment */}
-            <p className="comments__comment-count">{comments.length} {comments.length > 1 ? "Comments" : "Comment"}</p>
+            <p className="comments__comment-count">
+              {comments.length} {comments.length > 1 ? "Comments" : "Comment"}
+            </p>
             {/* sort the comments from newest to oldest */}
-            {comments.sort((a, b) => b.timestamp - a.timestamp).map((comment) => (
-              <div key={comment.id} className="comments__wrapper">
-                <div className="comments__name-date-container">
-                  <p className="comments__name">{comment.name}</p>
-                  <p className="comment__date">{new Date(comment.timestamp).toLocaleDateString()}</p>
-                </div>
-
-                <p className="comments__comment">{comment.comment}</p>
-              </div>
-            ))}
+            {comments
+              .sort((a, b) => b.timestamp - a.timestamp)
+              // then map the sorted comments
+              .map((comment) => (
+                <Comment key={comment.id} comment={comment} />
+              ))}
           </div>
         )}
       </div>
