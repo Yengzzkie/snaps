1e89b6ff200929094e0b7390e54492d663331556
Yengzzkie <gatchalian.manuel@gmail.com>
2025-03-04 20:58:47 -0500

    removed data folder and files

--- src/components/PhotosGrid/PhotosGrid.jsx
+++ src/components/PhotosGrid/PhotosGrid.jsx
@@ -5,22 +5,13 @@ import "./PhotosGrid.scss";
 const PhotosGrid = ({ photos, selectedTag, isOpen }) => {
   return (
     <section className={`card-container ${isOpen ? "two-cols" : ""}`}>
-      {photos?.length === 0 ? (
-        <p className="no-result-text">No results found</p>
-      ) : (
-        photos
-          ?.filter(
-            (photo) => photo.tags.includes(selectedTag) || selectedTag === null
+      {photos?.length === 0 ? (<p className="no-result-text">No results found</p>) : (
+        photos?.filter((photo) => photo.tags.includes(selectedTag) || selectedTag === null).map((photo) => ( // directly filter the photos based on the selected tag as per feedback
+          <Link key={photo.id} to={`/${photo.id}`}>
+            <Card photo={photo} />
+          </Link>
           )
-          .map(
-            (
-              photo // directly filter the photos based on the selected tag as per feedback
-            ) => (
-              <Link key={photo.id} to={`/${photo.id}`}>
-                <Card photo={photo} />
-              </Link>
-            )
-          )
+        )
       )}
     </section>
   );
